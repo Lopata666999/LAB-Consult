c19d5c65ece6b9b3cb22e27763aa6da541616a20
Aleks <Ast1945@rambler.ru>
2018-08-04 14:35:31 +0700

    Scroll btn

--- LAB Consulting/js/main.js
+++ LAB Consulting/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
 
+	// Прокрутка к следущей секции
+	$('.scrollBtn').click(function(act) {
+		act.preventDefault();
+		elementCick = $(this).attr('href');
+		destination = $(elementCick).offset().top;
+		$('body, html').animate({scrollTop: destination}, 800);
+	});
 });
